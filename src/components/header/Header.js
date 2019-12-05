97c2050fe1be7d9762d4f2da9de2033ea6e06019
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-05 07:12:15 -0500

    added Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>Recipe App</h1>
+            <p>Keep track of all your favorite recipes</p>
+        </header>
+    )
+}
+
+export default Header
