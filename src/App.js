0c9bccfde56f69f89872e19ce3b02f4f6246daf3
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-05 19:21:57 -0500

    set up routes and added edit component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+// Components
 import Header from './components/header/Header';
 import RecipeList from './components/recipe/RecipeList';
+import EditRecipe from './components/edit/EditRecipe';
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <RecipeList />
+      <Header />
+      <Route exact path='/' component={RecipeList} />
+      <Route path='/edit' component={EditRecipe} />
     </div>
   )
 }
