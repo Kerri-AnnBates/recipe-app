40d946535278096a0399da893332af346d124d3f
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-22 18:08:52 -0800

    refacted structure for full stack

---
+++ app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+import data from './data';
+
+// Components
+import Header from './components/header/Header';
+import RecipeList from './components/recipe/RecipeList';
+import EditRecipe from './components/edit/EditRecipe';
+
+// Contexts
+const RecipeContext = React.createContext();
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Route exact path='/' component={RecipeList} />
+      <Route path='/edit' component={EditRecipe} />
+    </div>
+  )
+}
+
+export default App;
