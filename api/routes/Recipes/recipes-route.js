05d52103e7bee8615d0d13bfab2d15f1e766c84c
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-09 15:29:21 -0500

    added a route for recipes

--- api/routes/Recipes/recipes-route.js
+++ api/routes/Recipes/recipes-route.js
@@ -6,9 +6,25 @@ router.get('/', (req, res) => {
       .then(recipes => {
          res.status(200).json(recipes);
       })
-      .catch(error => {
-         res.status(500).json({message: 'Problems fetching recipes'});
+      .catch(err => {
+         res.status(500).json({ message: 'Problems fetching recipes', error: err });
       })
 });
+
+router.get('/:id', (req, res) => {
+   const id = req.params.id;
+
+   Recipes.findById(id)
+      .then(recipe => {
+         if (recipe) {
+            res.status(200).json(recipe);
+         } else {
+            res.status(404).json({ message: 'No recipe exist by that id.' });
+         }
+      })
+      .catch(err => {
+         res.status(500).json({ message: 'Problems fetching recipe', error: err });
+      })
+})
 
 module.exports = router;
