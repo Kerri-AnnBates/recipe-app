5d738a29251c8fbc775943f6f3312e377c3874db
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-09 15:12:11 -0500

    set up initial functions for recipes model.

--- api/routes/Recipes/recipes-model.js
+++ api/routes/Recipes/recipes-model.js
@@ -1,9 +1,36 @@
 const db = require('../../../database/dbConfig');
 
 module.exports = {
-   get
+   get,
+   findById,
+   insert,
+   remove,
+   update
 }
 
 function get() {
    return db('recipes');
+}
+
+function findById(id) {
+   return db('recipes')
+      .where('id', id)
+      .first();
+}
+
+function insert(data) {
+   return db('recipes')
+      .insert(data);
+}
+
+function remove(id) {
+   return db('recipes')
+      .where('id', id)
+      .del();
+}
+
+function update(data, id) {
+   return db('recipes')
+      .where('id', id)
+      .update(data);
 }
