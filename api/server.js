6b86fc44269dfe402ad19c1e3a9d90640b2d632d
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-10 15:05:09 -0500

    add auth route to server

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 
 // Routes
 const userRoute = require('./routes/Users/users-routes');
 const recipeRoute = require('./routes/Recipes/recipes-route');
+const authRoute = require('./routes/Auth/auth-routes');
 
 server.use(express.json());
+server.use(cors());
 server.use('/api/users', userRoute);
 server.use('/api/recipes', recipeRoute);
+server.use('/api/auth', authRoute);
 
 server.get('/', (req, res) => {
    res.send('Fetch me some recipes!');
