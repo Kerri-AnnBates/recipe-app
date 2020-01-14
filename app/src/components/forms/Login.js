a101161a1b571b37693a3d5f5fc32f80d5591c4d
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-14 18:23:59 -0500

    minor styles to login form

--- app/src/components/forms/Login.js
+++ app/src/components/forms/Login.js
@@ -6,24 +6,30 @@ import Header from '../header/Header';
 
 function Login() {
    return (
-      <div className="container">
-         <Header />
-         <div className="login">
-            <h1>Log in</h1>
-            <Form>
-               <FormGroup>
-                  <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-               </FormGroup>
-               <FormGroup>
-                  <Label for="examplePassword">Password</Label>
-                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-               </FormGroup>
-               <Button>Login</Button>
-               <Button>Register</Button>
-               </Form>
+      <>
+         <div className="container">
+            <Header />
          </div>
-      </div>
+         <div className="login">
+            <div className="container">
+               <div className="login-wrapper">
+                  <h1>Log in</h1>
+                  <Form>
+                     <FormGroup>
+                        <Label for="exampleEmail">Email</Label>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                     </FormGroup>
+                     <FormGroup>
+                        <Label for="examplePassword">Password</Label>
+                        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                     </FormGroup>
+                     <Button>Login</Button>
+                     <Button>Register</Button>
+                  </Form>
+               </div>
+            </div>
+         </div>
+      </>
    )
 }
 
