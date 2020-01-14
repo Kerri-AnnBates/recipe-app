c437a1088a5641628f107092c7c04141e5447058
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-14 18:14:56 -0500

    added log in form

--- app/src/components/forms/Login.js
+++ app/src/components/forms/Login.js
@@ -9,7 +9,19 @@ function Login() {
       <div className="container">
          <Header />
          <div className="login">
-            
+            <h1>Log in</h1>
+            <Form>
+               <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+               </FormGroup>
+               <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+               </FormGroup>
+               <Button>Login</Button>
+               <Button>Register</Button>
+               </Form>
          </div>
       </div>
    )
