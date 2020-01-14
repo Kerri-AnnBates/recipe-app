bb42408aa63eb78a311c92d24c0b3437a4ba5f69
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-14 18:35:39 -0500

    styles for form

--- app/src/components/forms/Login.js
+++ app/src/components/forms/Login.js
@@ -16,15 +16,15 @@ function Login() {
                   <h1>Log in</h1>
                   <Form>
                      <FormGroup>
-                        <Label for="exampleEmail">Email</Label>
-                        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="username">Username:</Label>
+                        <Input type="username" name="username" id="username" placeholder="Enter your username" />
                      </FormGroup>
                      <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                        <Label for="password">Password:</Label>
+                        <Input type="password" name="password" id="password" placeholder="Enter your password" />
                      </FormGroup>
-                     <Button>Login</Button>
-                     <Button>Register</Button>
+                     <Button color="primary">Login</Button>
+                     <Button color="secondary">Register</Button>
                   </Form>
                </div>
             </div>
