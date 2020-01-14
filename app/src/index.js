8b2aa27dddccdc84829bc890f99509f1851261b4
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-14 18:11:24 -0500

    adjusted some styles to override bootstrap

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.scss';
 
 ReactDOM.render(
     <Router>
