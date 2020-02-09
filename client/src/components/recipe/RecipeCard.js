2c37e771fd708e176431ce0733b0f028186238a2
Kerri-Ann <kaybee1jam@yahoo.com>
2020-02-09 12:39:06 -0500

    added base modal for recipe card

--- client/src/components/recipe/RecipeCard.js
+++ client/src/components/recipe/RecipeCard.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import React, { useState } from 'react';
+import RecipeModal from './RecipeModal';
+import { Card, Button, CardTitle, CardText, CardBody, CardLink } from 'reactstrap';
 
 function RecipeCard(props) {
 	const { recipe } = props;
 
+	const [modal, setModal] = useState(false);
+
+	const toggle = () => setModal(!modal);
+
 	return (
 		<>
 			<Card body className="recipe-card">
-				<CardTitle>{recipe.title}</CardTitle>
-				<CardText>{recipe.description}</CardText>
+				<CardBody className="card-titles" onClick={toggle}>
+					<CardTitle>{recipe.title}</CardTitle>
+					<CardText>{recipe.description}</CardText>
+				</CardBody>
+				<CardBody className="card-links">
+					<CardLink href="#">Edit</CardLink>
+				</CardBody>
 			</Card>
+			<RecipeModal toggle={toggle} modal={modal} />
 		</>
 	)
 }
