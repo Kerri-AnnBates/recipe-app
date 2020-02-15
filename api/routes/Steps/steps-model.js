ce78e8a8fcf5e24a4d8fac867c637f51a6ba4e7f
Kerri-Ann <kaybee1jam@yahoo.com>
2020-02-15 17:07:52 -0500

    add steps route

--- api/routes/Steps/steps-model.js
+++ api/routes/Steps/steps-model.js
@@ -1,10 +1,16 @@
 const db = require('../../../database/dbConfig');
 
 module.exports = {
-   getStepsByRecipe
+	getStepsByRecipe,
+	insert
 }
 
 function getStepsByRecipe(id) {
-   return db('steps')
-      .where('recipe_id', id);
+	return db('steps')
+		.where('recipe_id', id);
+}
+
+function insert(data) {
+	return db('steps')
+		.insert(data);
 }
--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const userRoute = require('./routes/Users/users-routes');
 const recipeRoute = require('./routes/Recipes/recipes-route');
 const authRoute = require('./routes/Auth/auth-routes');
 const ingreientsRoute = require('./routes/Ingredients/ingredients-route');
+const stepsRoute = require('./routes/Steps/steps-route');
 
 server.use(express.json());
 server.use(cors());
@@ -14,5 +15,6 @@ server.use('/api/users', userRoute);
 server.use('/api/recipes', recipeRoute);
 server.use('/api/ingredients', ingreientsRoute);
 server.use('/api/auth', authRoute);
+server.use('/api/steps', stepsRoute);
 
 module.exports = server;
